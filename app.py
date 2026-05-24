1df83f74d5d5fbf4a0ecc1f103cc85f421bb3481
Medwin Moosa LeoFrix <moosairfaan0325@outlook.com>
2026-05-23 23:27:33 -0400

    Fix CORS in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
